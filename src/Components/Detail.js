453c6823917691cb131ec8c378243a6994677c05
ChaeSooyeon <cotndus3@gmail.com>
2021-10-18 23:15:56 +0900

    목록보기 버튼 생성

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Detail({location}){
     const data = location.state.data;
     return(
        <div className="content">
+          <div className="">
+            <Link to="/">
+                <button>&lt; 목록</button>
+            </Link>
+          </div>
           <div>제목 : {data.title} </div>
           <div>작성자 : {data.writer}  |  작성일 : {data.write_date}</div>
           <div>내용 : {data.content} </div>
--- src/Components/Tr.js
+++ src/Components/Tr.js
@@ -5,7 +5,7 @@ function Tr({data}){
     return(
         <tr>
             <td className="number">{data.id}</td>
-            <td className="title"><Link to={{pathname:'/Detail/'+data.id, state:{data}}}>{data.title}</Link></td>
+            <td className="title"><Link to={{pathname:'/Detail/'+data.id, state:{data}} }>{data.title}</Link></td>
             <td className="writer">{data.writer}</td>
             <td className="write_date">{data.write_date}</td>
         </tr>
